43dbdc01e6b53c4440664e02a6813a5f0c8c37dc
Dino Arla <dinoprojectsid@gmail.com>
2020-01-16 11:19:07 +0700

    Admin Login

--- admin/login.php
+++ admin/login.php
@@ -4,76 +4,85 @@ include "../library/config.php";
 include "../library/function_antiinjection.php";
 ?>
 
+<!DOCTYPE HTML>
 <html>
 <head>
-	<title>Halaman Administrator</title>
-	
-	<meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.0" />
-
-	<link rel="stylesheet" type="text/css" href="../plugin/bootstrap/css/bootstrap.min.css"/>
-	<link rel="stylesheet" type="text/css" href="css/login.css"/>
-	
-	<script type="text/javascript" src="../plugin/jquery/jquery-2.0.2.min.js"></script>
+<title>Fakultas Ilmu Komunikasi Universitas Pancasila - Halaman Administrator</title>
+<link rel="shortcut icon" href="images/logo.png" />
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/login.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- for-mobile-apps -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="#" />
+<!-- //for-mobile-apps -->
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
-
-<div class="container-fluid"> 	
-	<div class="row">
-		<div class="col-md-4 col-md-push-4 form-login">
+<!--header start here-->
+<div class="body"></div>
+<div class="header">
 		
-<?php
-if(isset($_POST['login'])){
-	$username = antiinjeksi($_POST['username']);
-	$password = antiinjeksi(md5($_POST['password']));
+						
+						<?php
+						if(isset($_POST['login'])){
+							$username = antiinjeksi($_POST['username']);
+							$password = antiinjeksi(md5($_POST['password']));
 
-	$cekuser = $mysqli->query("SELECT * FROM user WHERE username='$username' AND password='$password'");
-	$jmluser = $cekuser->num_rows;
-	$data = $cekuser->fetch_array();
+							$cekuser = $mysqli->query("SELECT * FROM user WHERE username='$username' AND password='$password'");
+							$jmluser = $cekuser->num_rows;
+							$data = $cekuser->fetch_array();
 
-	if ($jmluser> 0){
+							if ($jmluser> 0){	
 
-	  $_SESSION['username']     = $data['username'];
-	  $_SESSION['namalengkap']  = $data['nama_lengkap'];
-	  $_SESSION['password']     = $data['password'];
-	  $_SESSION['iduser']     	= $data['id_user'];
-	  $_SESSION['leveluser']    = $data['level'];
-	  
-	  $_SESSION['timeout'] = time()+1000;
-	  $_SESSION['login'] = 1;
-	  
-	  header('location: index.php');
-	}else{
-	  echo'<div class="alert alert-danger login-alert" role="alert"><b>Sorry!</b> Username atau password salah.</div>';
-	}
-}
-?>
+							  $_SESSION['iduser']     	= $data['id_user'];
+							  $_SESSION['namalengkap']  = $data['nama_lengkap'];
+							  $_SESSION['persnumber']  	= $data['pers_number'];
+							  $_SESSION['leveluser']    = $data['level'];
+							  $_SESSION['username']     = $data['username'];
+							  $_SESSION['password']     = $data['password'];
+							  
+							  $_SESSION['timeout'] = time()+1000;
+							  $_SESSION['login'] = 1;
+							  
+							  header('location: index.php');
+							}else{
+							  echo'<div class="alert" role="alert"><b>Sorry!</b> Username atau password salah.</div>';
+							}
+						}
+						?>
 
-			<div class="panel panel-primary">
-			   <div class="panel-heading">
-				  <b>Login Administrator</b>
-			   </div>
-			   <div class="panel-body">
-				  <form method="post" action="login.php" class="form-horizontal">
-					<div class="input-group">
-						<div class="input-group-addon"><i class="glyphicon glyphicon-user"></i></div>
-						<input type="text" name="username" placeholder="Username" autofocus class="form-control">
-					</div>
-					<br/>
-					<div class="input-group">
-						<div class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></div>
-						<input type="password" name="password" placeholder="Password" class="form-control">
-					</div>
-					<br/>
-					<button type="submit" name="login" class="btn btn-primary pull-right">
-						<i class="glyphicon glyphicon-log-in"></i> Login Administrator
-					</button>
-				  </form>
-			   </div>
+						<div class="header-main">
+				<img class="logo" src="images/logo.png"></img>
+		       <h1>ADMINISTRATOR</h1>
+		       <h4>FIKOM UNIVERSITAS PANCASILA</h4>
+			<div class="header-bottom">
+				<div class="header-right w3agile">
+					
+					<div class="header-left-bottom agileinfo">
+
+					 <form action="login.php" method="post">
+						<input type="text"  value="Username" name="username" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Username';}"/>
+					<input type="password"  value="Password" name="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}"/>
+					   
+						<input type="submit" value="Login" name="login">
+					</form>	
+				</div>
+				</div>
+			  
 			</div>
 		</div>
-	</div>
 </div>
-
+<!--header end here-->
+<div class="copyright">
+	<p>Fakultas Ilmu Komunikasi Universitas Pancasila &copy; <script>document.write(new Date().getFullYear());</script>. All rights reserved.</p>
+</div>
+<!--footer end here-->
 </body>
 </html>
+
+	
+    
