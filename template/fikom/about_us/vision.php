0a8b64ed445b2a8fa90782198619872f34fdca5d
Amos Christian <amos.christian@hreasily.com>
2020-02-18 09:45:57 +0700

    Changes v4

--- template/fikom/about_us/vision.php
+++ template/fikom/about_us/vision.php
@@ -1,5 +1,4 @@
 <div class="news_post">
-    <span>Vision</span>
     <div class="news_post_quote">
         <p class="news_post_quote_text">
             <span>P</span>ada tahun 2019, program studi ilmu komunikasi menjadi unggul dan kompetitif dalam bidang jurnalistik multimedia, komunikasi strategis, dan kajian media berdasarkan Pancasila. 
