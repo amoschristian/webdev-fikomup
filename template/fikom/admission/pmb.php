f196b022204c62a81a3e3dee1addd76c115de584
Amos Christian <amos.christian@hreasily.com>
2020-02-23 23:26:13 +0700

    Responsive PMB

--- template/fikom/admission/pmb.php
+++ template/fikom/admission/pmb.php
@@ -1,8 +1,15 @@
 <div class="news_post">
-    <img src="/media/source/alur.jpg"></img>
+    <img src="/media/source/alur.jpg" class="responsive"></img>
     <div class="news_post_quote">
         <p class="news_post_quote_text">
             <a href="http://pmbonline.univpancasila.ac.id/" style="color:#1a1a1a" target="_blank"><span>C</span>lick Here to go to PMB Online</a>
         </p>
     </div>
 </div>
+
+<style>
+.responsive {
+  width: 100%;
+  height: auto;
+}
+</style>
