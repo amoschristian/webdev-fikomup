0b41689038fe1c5dbde996d4a115c1344d7d7a17
Amos Christian <amos.christian@hreasily.com>
2020-03-24 20:26:59 +0700

    Update IG Feed box

--- template/fikom/template/headline_home.php
+++ template/fikom/template/headline_home.php
@@ -67,7 +67,7 @@ while ($data = $sub_news_query->fetch_array(MYSQLI_ASSOC)) {
                     <div class="ig-feed-box" id="instafeed">
                         <!-- SnapWidget -->
                         <script src="https://snapwidget.com/js/snapwidget.js"></script>
-                        <iframe src="https://snapwidget.com/embed/805258" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:100%; "></iframe>
+                        <iframe src="https://snapwidget.com/embed/805258" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:95%; "></iframe>
                     </div>
                 </div>
             </div>
