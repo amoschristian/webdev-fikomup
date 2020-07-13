9e96363fd22421777dc720771992e78433252bb0
Amos Christian <amos.christian@hreasily.com>
2020-07-13 20:46:18 +0700

    fix activity migration

--- migrations/activity.sql
+++ migrations/activity.sql
@@ -10,26 +10,48 @@
 /*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `activity`
+-- Table structure for table `activity`
 --
 
-LOCK TABLES `activity` WRITE;
-/*!40000 ALTER TABLE `activity` DISABLE KEYS */;
-INSERT INTO `activity` VALUES (1,'','Test','','','<p>Test</p>','','Xhoth009.jpg,stubbedout1.jpg,struktur_organisasi.jpg','Senin','2020-07-13','19:56:56',1,0,'',0,NULL,'2020-07-13 19:56:20','2020-07-13 19:56:56'),(2,'','Test 2','','','<p>Test</p>','','','Senin','2020-07-13','20:01:36',1,0,'',0,NULL,'2020-07-13 20:01:36',NULL);
-/*!40000 ALTER TABLE `activity` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `activity` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `judul` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `judul_terjemahan` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `judul_seo` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `isi` text COLLATE latin1_general_ci NOT NULL,
+  `isi_terjemahan` text COLLATE latin1_general_ci NOT NULL,
+  `lokasi` text COLLATE latin1_general_ci NOT NULL,
+  `gambar` text COLLATE latin1_general_ci NOT NULL,
+  `hari` varchar(20) COLLATE latin1_general_ci NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` time NOT NULL,
+  `id_user` int(5) NOT NULL,
+  `kategori` int(5) NOT NULL,
+  `tag` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `hits` int(10) NOT NULL,
+  `headline` int(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-07-13 20:36:46
+-- Dump completed on 2020-07-13 20:45:29
