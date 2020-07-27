8a3acdda987a2c151c626b52d9ee03555de56484
Amos Christian <amos.christian@hreasily.com>
2020-07-27 23:45:55 +0700

    Change SMTP pass

--- email.php
+++ email.php
@@ -43,7 +43,7 @@ function send_mail($subject, $message, $from_email, $from_name){
         $mail->isSMTP();                                      // Send using SMTP
         $mail->Host         = 'smtp-relay.sendinblue.com';    // Set the SMTP server to send through
         $mail->Username     = 'fikomup.dev@gmail.com';        // SMTP username
-        $mail->Password     = 'VjaHZ4Yc3fEUrpqt';             // SMTP password
+        $mail->Password     = 'xsmtpsib-cd3e3c86df295e5b36fb669f8fc50470a3fd7e8bce015d47a056f378ef11b284-49IgMPOZ0FVscUDr';             // SMTP password
         $mail->SMTPSecure   = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port         = 587;                            // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         $mail->SMTPAuth     = true;                           // authentication enabled
