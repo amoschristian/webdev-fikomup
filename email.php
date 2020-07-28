077ade3f47b3d57fc43773b253c3b334425c6d94
Amos Christian <amos.christian@hreasily.com>
2020-07-28 19:56:21 +0700

    update email

--- email.php
+++ email.php
@@ -34,30 +34,35 @@ function sanitize_my_email($field) {
 
 function send_mail($subject, $message, $from_email, $from_name){
     $mail = new PHPMailer(true);
+    $sender_email = 'feedback@fikomup.com';
+    $sender_name = 'Inquiry/Feedback';
     $to_email = 'inquiry@fikomup.com';
-    $to_name = 'Inquiry';
-    
+    $to_name = 'Inquiry/Feedback';
+
     try {
         //Server settings
-        $mail->SMTPDebug    = SMTP::DEBUG_OFF;                // Enable verbose debug output
-        $mail->isSMTP();                                      // Send using SMTP
-        $mail->Host         = 'smtp-relay.sendinblue.com';    // Set the SMTP server to send through
-        $mail->Username     = 'fikomup.dev@gmail.com';        // SMTP username
-        $mail->Password     = 'xsmtpsib-cd3e3c86df295e5b36fb669f8fc50470a3fd7e8bce015d47a056f378ef11b284-49IgMPOZ0FVscUDr';             // SMTP password
-        $mail->SMTPSecure   = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port         = 587;                            // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-        $mail->SMTPAuth     = true;                           // authentication enabled
+        $mail->SMTPDebug    = SMTP::DEBUG_OFF;               // Enable verbose debug output
+        $mail->isSMTP();                                        // Send using SMTP
+        $mail->Host         = 'srv92.niagahoster.com';          // Set the SMTP server to send through
+        $mail->Username     = 'feedback@fikomup.com';           // SMTP username
+        $mail->Password     = 'bidang03';                       // SMTP password
+        $mail->SMTPSecure   = PHPMailer::ENCRYPTION_STARTTLS;   // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port         = 587;                              // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $mail->SMTPAuth     = true;                             // authentication enabled
 
         //Recipients
-        $mail->setFrom($from_email, $from_name);
+        $mail->setFrom($sender_email, $sender_name);
         $mail->addAddress($to_email, $to_name);       // Add a recipient
         // $mail->addCC('cc@example.com');                          //Add CC
 
         // Content
         $mail->isHTML(true);                                  // Set email format to HTML
         $mail->Subject = $subject;
-        $mail->Body    = $message;
-        $mail->AltBody = $message;
+
+        $from_message = "$from_email ($from_name) telah mengirimkan pesan <br><br>";
+        $from_message .= $message;
+        $mail->Body    = $from_message;
+        $mail->AltBody = $from_message;
 
         $mail->send();
         echo "sent";
