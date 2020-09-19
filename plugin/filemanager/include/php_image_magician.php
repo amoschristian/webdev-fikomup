8d70f161409b4a47f5ee5332a1f8a326c4971075
Amos Christian <amos.christian@hreasily.com>
2020-09-19 10:54:31 +0700

    Fix upload

--- plugin/filemanager/include/php_image_magician.php
+++ plugin/filemanager/include/php_image_magician.php
@@ -2723,7 +2723,7 @@ class imageLib {
 		$extension = mime_content_type($file);
 		$extension = fix_strtolower($extension);
 		$extension = str_replace('image/', '', $extension);
-		ini_set("memory_limit","500M");
+		ini_set("memory_limit","1000M");
 		switch ($extension)
 		{
 			case 'jpg':
