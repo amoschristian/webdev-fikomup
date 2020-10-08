41c9bc3c7bb708c1ffbf08f51f2a3c7bab62a2c1
Amos Christian <amos.christian@hreasily.com>
2020-10-08 17:25:44 +0700

    Fix instagram token expired

--- admin/content/instagram.php
+++ admin/content/instagram.php
@@ -28,9 +28,9 @@ $token_display =  str_repeat("*", strlen($token)-10) . substr($token, -10);
     <thead><tr><th colspan="2"><h3><b>Instagram</b></h3></th></tr></thead>
     <tbody>
     <?php if($profile) : ?>
-    <tr><td><b>Akun</b></td>	<td>: <?=  $profile->username ?></td></tr>
-    <tr><td><b>Tipe Akun</b></td>	<td>: <?=  $profile->account_type ?></td></tr>
-    <tr><td><b>Jumlah Gambar</b></td>	<td>: <?=  $profile->media_count ?></td></tr>
+    <tr><td><b>Akun</b></td>	<td>: <?=  (isset($profile->username) ? $profile->username : "-") ?></td></tr>
+    <tr><td><b>Tipe Akun</b></td>	<td>: <?=  (isset($profile->account_type) ? $profile->account_type : "-") ?></td></tr>
+    <tr><td><b>Jumlah Gambar</b></td>	<td>: <?=  (isset($profile->media_count) ? $profile->media_count : "-") ?></td></tr>
     <?php endif; ?>
     <tr><td><b>Token</b></td>	<td>: <?=  $token_display ?></td></tr>
     <tr><td><b>Tanggal Kadaluarsa Token</b></td>	<td>: <?=  strftime("%d %B %Y", strtotime($instagram->token_expiration)); ?></td></tr>
